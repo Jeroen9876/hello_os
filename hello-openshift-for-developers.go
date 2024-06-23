aca37eac167aec9e948fdcbfc4319177b47b4cfe
Jeroen9876 <53210934+Jeroen9876@users.noreply.github.com>
2024-06-23 20:42:39 +0200

    Update hello-openshift-for-developers.go
    Hello world tekst vertaald naar het Nederlands. Groet is nu " Hallo Openshift voor ontwikkelaars!"

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift for Developers!"
+		response = "Hallo OpenShift voor ongeduldige ontwikkelaars!"
 	}
 
 	fmt.Fprintln(w, response)
